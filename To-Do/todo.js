3c2e41c57b560afa4ff037e3dcac3455054b2615
Surya Prabhat Saurabh <154997765+suryaprabhat-saurabh@users.noreply.github.com>
2026-02-21 23:30:01 +0530

    Learned Gradient - Added logo for Portfolio

---
+++ To-Do/todo.js
@@ -0,0 +1,44 @@
+// let todo = [];
+// let createBtn = document.querySelector("#creatingTodo");
+// let deleteAll = document.querySelector("#deletingTodo");
+
+// let body = document.querySelector("body");
+
+// let ul = document.createElement("ul");
+
+// function createTodo() {
+//   todo.push(todoInput.value);
+  
+//     let li = document.createElement("li");
+//     let deleteBtn = document.createElement("button");
+//     deleteBtn.innerText = "Delete";
+//     deleteBtn.style.margin = "10px";
+//     deleteBtn.style.background = "red";
+    
+//     li.innerText = todo[0];
+//     ul.append(li, deleteBtn);
+    
+//     li.style.cursor = "pointer";
+  
+    
+//     deleteBtn.addEventListener("click", function () {
+//       li.remove();
+//       deleteBtn.remove();
+//   });
+
+//   // Event
+
+//   li.addEventListener("click", function () {
+//     li.classList.toggle("line");
+//   });
+//   // todo = [];
+//   todoInput.value = "";
+// }
+
+// // Append to Body
+// function deleteAllTodo() {
+//   ul.remove();
+// }
+
+// body.append(ul);
+// let todoInput = document.querySelector("#inputField");
